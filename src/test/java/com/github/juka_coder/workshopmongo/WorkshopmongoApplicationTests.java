f5cc6a68ad059d7bd5824abbbdbb1d1cd9a32c2b
Juka-coder <pilaojxjd@gmail.com>
2025-09-01 21:41:36 -0300

    first commit

---
+++ src/test/java/com/github/juka_coder/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juka_coder.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
