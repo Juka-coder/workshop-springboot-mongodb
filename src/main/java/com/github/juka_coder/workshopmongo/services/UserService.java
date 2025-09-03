bcc0a17f56583889b7f0441611a5438f57ece288
Juka-coder <pilaojxjd@gmail.com>
2025-09-03 11:19:54 -0300

    User deletion with DELETE

--- src/main/java/com/github/juka_coder/workshopmongo/services/UserService.java
+++ src/main/java/com/github/juka_coder/workshopmongo/services/UserService.java
@@ -29,6 +29,11 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public void delete(String id){
+        findById(id);
+        repo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDto){
         return new User(objDto.getName(),objDto.getId(), objDto.getEmail());
     }
